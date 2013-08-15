8e4cf51fc43ea22488ac0dbbb63e6a12c337472b
Jon Williams <jon@jonathannen.com>
2013-08-15 17:43:23 +1000

    Escape shell commands

--- lib/poi2csv.rb
+++ lib/poi2csv.rb
@@ -1,9 +1,12 @@
+require 'shellwords'
 require "poi2csv/version"
 
 module Poi2csv
 
   def self.to_csv(input_file_path, output_folder_path, separator=nil, formating_convention=nil) 
-  	`java -cp #{classpath} ToCSV #{input_file_path} #{output_folder_path} #{separator} #{formating_convention}`
+    args = [input_file_path, output_folder_path, separator, formating_convention].reject { |v| v.nil? }
+    args = args.map { |v| Shellwords.escape(v) }
+  	`java -cp #{classpath} ToCSV #{args * ' '}`
   end
 
   def self.classpath
