350bb8aa3e4b13b72db89b6bd42fafb4104d6470
Sushma Girish <sushma@cultureamp.com>
2014-12-09 16:54:44 +1100

    Add a list of supported extensions

--- lib/poi2csv.rb
+++ lib/poi2csv.rb
@@ -4,6 +4,7 @@ require "poi2csv/version"
 module Poi2csv
 
   Poi2csvException = Class.new(StandardError)
+  SUPPORTED_EXTENSIONS = [:xls, :xlsx]
 
   def self.to_csv(input_file_path, output_folder_path, separator=nil, formating_convention=nil)
     args = [input_file_path, output_folder_path, separator, formating_convention].reject { |v| v.nil? }
